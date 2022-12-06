28d92423e8fae9938e7c1d2d1c7e3ee4da883900
LouisMaupas <louismaupas@gmail.com>
2022-12-06 11:37:30 +0100

    working on classes

---
+++ premierProjet/premierProjet/Library/Class1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace premierProjet.Library
+{
+    public class Player
+    {
+        public string name; // 
+        public Deck deck;
+        public byte score;
+        public object hand;
+
+        public Player(string name, Deck deck, byte score, object hand) // constructor
+        {
+            this.name = name;
+            this.deck = deck;
+            this.score = score;
+            this.hand = hand;
+        }
+
+        public string Name { get { return name; } }
+
+        public Deck Deck { 
+            get { return deck; }
+            set { deck = value; }
+        }
+
+        public byte Score { 
+            get { return score; }
+            set
+            {
+                this.score = value;
+            }}
+
+        public object Hand { 
+            get { return hand; }
+            set { hand = value; }
+        }
+
+    }
+
+    public class Deck
+    {
+        public Array deck;
+        public Deck(Array deck)
+        {
+            this.deck = deck;
+        }
+
+        public Array CurrentDeck
+        {
+            get { return deck; }
+            set { deck = value; }
+        }
+    }
+
+    //public class Card
+    //{
+    //    public string name;
+    //    public byte value;
+
+    //    public Card(string name, byte value)
+    //    {
+    //        this.name = name;
+    //        this.value = value;
+    //    }
+
+    //    public object Value
+    //    {
+    //        get { return value; }
+    //    }
+
+    //    public object Name
+    //    {
+    //        get { return name; }
+    //    }
+    //}
+
+    //enum Cards
+    //{
+    //    As = 1,
+    //    Two = 2,
+    //    Three = 3,
+    //    Four = 4,
+    //    Five = 5,
+    //    Six = 6,
+    //    Seven = 7,
+    //    Eight = 8,
+    //    Nine = 9,
+    //    Ten = 10,
+    //    Jack = 11,
+    //    Queen = 12,
+    //    King = 13,
+    //}
+
+}
---
+++ premierProjet/premierProjet/Program.cs
@@ -0,0 +1,45 @@
+﻿// explication des regles
+// on crée un nouveau joueur qui crée un nouveau deck qui cree des cards
+// Tant que le score du player 1 + score player 2 =/= 5 on boucle
+/* A chaque tour :
+ *      - P1 retire une card de son deck et l'ajoute à sa main
+ *      - La main est affiché à l'écran
+ *      - P1 choisit une carte : elle est retiré de la main
+ *      - P2 tire aléatoirement une carte
+ *      - VS des cartes 1 score s'incrémente
+ */
+
+using premierProjet.Library;
+
+// intro + get player name
+Console.WriteLine("C'est un jeu de carte très basique, vous affrontez le pc");
+Console.WriteLine("Votre deck est composé des 13 cartes d'un jeu classique (sans couleur)");
+Console.WriteLine("Le premier arrivé à 3 points gagne la partie");
+Console.WriteLine("Comme vous tirez une nouvelle carte à chaque début de tour");
+Console.WriteLine("Et que votre main ne possède que 3 cartes vous ne piocherai pas toutes les cartes du deck");
+Console.WriteLine("Comment vous appellez vous ?");
+
+string playerName;
+playerName = Console.ReadLine(); // while pas tring
+
+// build cards and deck
+int[] Cards = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+Deck playerDeck = new Deck(Cards);
+Deck computerDeck = new Deck(Cards);
+
+
+// build players
+Player player = new Player(playerName, playerDeck, 0, null);
+Player computer = new Player("Ordinateur", computerDeck, 0, null);
+
+
+// Game
+while (player.score + computer.score < 5)
+{
+    Console.WriteLine(player.name + " a un score de : " + player.score);
+    Console.WriteLine(player.name + " la 1er carte dans son deck est : " + player.deck.CurrentDeck);
+
+    player.Score = (byte)(player.score + 1);
+}
+
+
